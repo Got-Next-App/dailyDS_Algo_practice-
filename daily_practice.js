e5b2382ee3b9bb30ee1e367616417f8edcdd4a49
Mari PD <48144818+maripd@users.noreply.github.com>
2023-04-25 11:39:00 -0500

    day 2 two sum practice

--- daily_practice.js
+++ daily_practice.js
@@ -1,3 +1,5 @@
+//April 24, 2023 Monday
+
 function findMinMax(arr) {
   let maxVal = arr[0]
   let minVal = arr[0]
@@ -17,3 +19,25 @@ const [minVal, maxVal] = findMinMax(arr)
 
 console.log(`Minimum value is: ${minVal}`)
 console.log(`Maximum value is: ${maxVal}`)
+
+//April 25, 2023 Tuesday
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// The problem is asking you to find two numbers in a list of numbers (array)
+// that add up to a specific number (target). The solution should return the index of each of
+// the two numbers in the original list. You cannot use the same number twice, and there is only one correct answer. You can present the answer in any order.
+
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 22))
